0e4afd3a497fba346d170f7a7db06069bd1a1264
luoChen <hank.luo@xianghuanji.com>
2020-04-20 11:40:41 +0800

    ..

--- app/src/main/java/com/niluogege/plugindemo/MyPathClassLoader.java
+++ app/src/main/java/com/niluogege/plugindemo/MyPathClassLoader.java
@@ -13,4 +13,25 @@ public class MyPathClassLoader extends PathClassLoader {
         super("", "", originCl.getParent());
         this.originCl = originCl;
     }
+
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+
+        try {
+            //先从插件中找
+            Class<?> aClass = Plugin.getPlugin(name).pluginCl.loadClass(name);
+            if (aClass == null) {
+                //再从之前的中找
+                aClass = originCl.loadClass(name);
+                if (aClass != null) {
+                    return aClass;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //最后从自己身上找
+        return super.loadClass(name, resolve);
+    }
 }
---
+++ app/src/main/java/com/niluogege/plugindemo/Plugin.java
@@ -0,0 +1,14 @@
+package com.niluogege.plugindemo;
+
+/**
+ * Created by niluogege on 2020/4/20.
+ */
+public class Plugin {
+    //插件使用的classLoader
+    public ClassLoader pluginCl;
+
+
+    public static Plugin getPlugin(String name) {
+        return null;
+    }
+}
